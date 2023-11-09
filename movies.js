b19f3fd2c71cd97e7b21d9071ab5f384bc37cb3d
Jeimnis_Bermejo <jeimnisbermejo@gmail.com>
2023-11-09 14:17:57 -0500

    actualizacion en movie. Js

--- movies.js
+++ movies.js
@@ -1,14 +1,21 @@
 const mostrarcard = document.getElementById("contenedor")
 
-movies.forEach(movie => {
-    const moviesCard = document.createElement("article");
-    moviesCard.innerHTML = `<img class=" object-contain rounded-xl " src="${movie.image}" alt="imagen"></img> 
-<h2 class= " text-white justify-items-start text-center p-1 font-bold  ">${movie.title}</h2> 
-<h3 class=" text-white text-left justify-items-start p-2 ">${movie.tagline}</h3>
-<p class=" text-white text-left justify-items-start">${movie.overview}</p>
-`
 
-    moviesCard.className = "flex flex-col  border  border-white-500   h-[33rem] w-[30rem] border-solid-5 p-8 rounded-lg mt-5  justify-center"
-    
-    mostrarcard.appendChild(moviesCard);
-});
+function card(peli){
+return `<article  class="flex flex-col  border  border-white-500  h-[33rem] w-[30rem] border-solid-5 p-8 rounded-lg mt-5  justify-center" >
+ <img class=" object-contain rounded-xl " src="${peli.image}" alt="imagen"></img> 
+<h2 class= " text-white justify-items-start text-center p-1 font-bold  ">${peli.title}</h2> 
+<h3 class=" text-white text-left justify-items-start p-2 ">${peli.tagline}</h3>
+<p class=" text-white text-left justify-items-start">${peli.overview}</p>
+</article>`
+}
+function agregarcard(array, contenedor) {
+    let movie = ""
+    for (const movies of array) {
+      const cards = card(movies)
+      movie += cards
+    }
+    contenedor.innerHTML += movie
+}
+agregarcard(movies, mostrarcard)
+
